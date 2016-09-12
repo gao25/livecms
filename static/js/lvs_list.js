00c5bb1f8ec2205bfbd544c0d357a019a22a39be
gaojunya <gaojunya@xinhuamm.net>
2016-09-12 09:28:37 +0800

    list

---
+++ static/js/lvs_list.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.datetime').datetimepicker({
+    timepicker:false,
+    format:'Y.m.d'
+  });
+  $('.more').mouseover(function(){
+    $('.more-menu').show();
+    $('.more img').attr('src','/static/img/green_menu.png');
+  })
+})
