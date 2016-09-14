285484e16b53901de870081ced1941b738599941
gaojunya <gaojunya@xinhuamm.net>
2016-09-14 19:31:34 +0800

    checklvs

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,15 @@
 $(function(){
+
+	/*首页 菜单项改变*/
+	var srcs=['lvslist.html','checklvs.html','',''];
+	$('.lvs-nav a').click(function(){
+		$('.lvs-nav a').removeClass('current');
+		$(this).addClass('current');
+		var i=$(this).parent().index();
+		$('.lvs-iframe iframe').attr('src',srcs[i]);
+	})
+  
+	/*个人设置样式*/
 	$('.lset').hover(function(){
 		$('.lset ul').show();
 	},function(){
