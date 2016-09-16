29b9dc902e2f129736c6d000a5fe703e6a991687
befish <xiaojia_fish@126.com>
2016-09-16 19:25:52 +0800

    front

--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,94 @@
-$(function(){
+var navConfig = [
+  {
+    "title": "现场管理",
+    "list": [
+      {
+        "menu": "待签报道",
+        "link": "/scene/reportlist.html"
+      },
+      {
+        "menu": "待签现场",
+        "link": "/scene/reportlist.html"
+      },
+      {
+        "menu": "成品现场",
+        "link": "/scene/reportlist.html"
+      },
+      {
+        "menu": "待审评论",
+        "link": "/scene/reportlist.html"
+      },
+      {
+        "menu": "视频素材",
+        "link": "/scene/reportlist.html"
+      }
+    ]
+  },
+  {
+    "title": "用户统计",
+    "list": [
+      {
+        "menu": "用户统计",
+        "link": "/scene/reportlist.html"
+      }
+    ]
+  },
+  {
+    "title": "数据统计",
+    "list": [
+      {
+        "menu": "数据统计",
+        "link": "/scene/reportlist.html"
+      }
+    ]
+  },
+  {
+    "title": "管理员管理",
+    "list": [
+      {
+        "menu": "管理员管理",
+        "link": "/scene/reportlist.html"
+      }
+    ]
+  }
+];
+var navObj = $('#j-nav'),
+  leftNavObj = $('#j-leftnav');
+$.each(navConfig, function(index){
+  // 创建顶部菜单
+  if (index > 0) {
+    navObj.append('<em>/</em>');
+  }
+  var newNav = $('<a href="javascript:void(0)">'+this['title']+'<em class="arrow"></em></a>'),
+    newLeftNavObj = $('<ul class="fn-hide"></ul>');
+  navObj.append(newNav);
+  // 创建左侧菜单
+  $.each(this['list'], function(){
+    newLeftNavObj.append('<li><a target="mainframe" href="'+this['link']+'"><em class="line"></em><em class="arrow"></em>'+this['menu']+'</a></li>');
+  });
+  leftNavObj.append(newLeftNavObj);
+  // 绑定顶部和左侧菜单联动
+  newNav.click(function(){
+    navObj.find('a').removeClass('current');
+    $(this).addClass('current');
+    leftNavObj.find('ul').addClass('fn-hide');
+    newLeftNavObj.removeClass('fn-hide');
+    newLeftNavObj.find('a').removeClass('current');
+    var firstA = newLeftNavObj.find('a:eq(0)');
+    firstA.addClass('current');
+    $('iframe[name=mainframe]').attr('src', firstA.attr('href'));
+    return false;
+  });
+  // 绑定左侧菜单样式变化
+  newLeftNavObj.find('a').click(function(){
+    newLeftNavObj.find('a').removeClass('current');
+    $(this).addClass('current');
+  });
+});
 
-	/*首页 菜单项改变*/
-	var srcs=['lvslist.html','checklvs.html','',''];
-	$('.lvs-nav a').click(function(){
-		$('.lvs-nav a').removeClass('current');
-		$(this).addClass('current');
-		var i=$(this).parent().index();
-		$('.lvs-iframe iframe').attr('src',srcs[i]);
-	})
-  
-	/*个人设置样式*/
-	$('.lset').hover(function(){
-		$('.lset ul').show();
-	},function(){
-		$('.lset ul').hide();
-	})
-	$('.lset li a').hover(function(){
-		$(this).css('color','#12bb9a');
-	},function(){
-		$(this).css('color','#f3f3f3');
-	})
-})
+// 默认展示菜单第一项
+navObj.find('a:eq(0)').addClass('current');
+leftNavObj.find('ul:eq(0)').removeClass('fn-hide');
+leftNavObj.find('a:eq(0)').addClass('current');
+$('iframe[name=mainframe]').attr('src', leftNavObj.find('a:eq(0)').attr('href'));
+
---
+++ static/js/login.js
@@ -0,0 +1,34 @@
+var newTplform = new cake["tplform-1.0.1"]('j-loginform'),
+formConfig = {
+  "type": "ajax",
+  "method": "post",
+  "action": "xxx",
+  "fields": [{
+    "class": "username",
+    "title": "用户名",
+    "name": "username",
+    "type": "text",
+    "required": true,
+    "minlength": 4,
+    "maxlength": 100,
+    "placeholder": "输入ID或用户名登录"
+  }, {
+    "class": "userpwd",
+    "title": "密码",
+    "name": "userpwd",
+    "type": "password",
+    "required": true,
+    "minlength": 4,
+    "maxlength": 100,
+    "placeholder": "输入密码"
+  }],
+  "button": [
+    {
+      "value": "登录",
+      "type": "submit"
+    }
+  ]
+};
+newTplform.render(formConfig, null, function(){
+  alert('ajax');
+});
