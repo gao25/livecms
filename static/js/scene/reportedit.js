fc08d1fa2f4070a78731caf70efef8a6911482f3
befish <xiaojia_fish@126.com>
2016-09-26 18:11:30 +0800

    v

--- static/js/scene/reportedit.js
+++ static/js/scene/reportedit.js
@@ -1,6 +1,6 @@
 // 参数
-var id = + lvsCmd['urlParams']['id'];
-if (isNaN(id) || id < 1) {
+var urlData = lvsCmd['urlParams'];
+if (isNaN(urlData['reportId']) || urlData['reportId'] < 1) {
   parent.location.href = '/';
 }
 
@@ -15,26 +15,22 @@ formConfig = {
     "title": "所属现场",
     "name": "topic",
     "type": "text",
-    "readonly": true
+    "disabled": true
   }, {
     "title": "报道人",
-    "name": "userid",
+    "name": "reporterId",
     "type": "select",
     "option": []
   }, {
     "title": "发布时间",
-    "name": "createtime",
+    "name": "createTime",
     "type": "datetime"
   }, {
+    "class": 'readonly j-type',
     "title": "稿件类型",
     "name": "type",
-    "type": "select",
-    "disabled": true,
-    "option": [
-      {"text": "图文", "value": "1"},
-      {"text": "音频", "value": "2"},
-      {"text": "视频", "value": "4"}
-    ]
+    "type": "text",
+    "disabled": true
   }, {
     "class": "uploader j-uploader",
     "title": "上传文件",
@@ -71,39 +67,45 @@ formConfig = {
   ]
 };
 newTplform.render(formConfig, function(){
-  // 创建上传组件
-  var newUpfile = new lvsCmd['upfile']($('.j-uploader .filelist'));
   // 取消
   $('.j-cancel').click(function(){
     history.back();
   });
-}, function(){
+}, function (formInfo) {
   // 获取表单数据
-  var formData = {
-    "id": id,
-    "createTime": new Date($('#j-editform input[name=createTime]').val()).getTime(),
-    "content": $('#j-editform textarea[name=content]').val()
-  };
-  var selectedIndex = $('#j-editform select[name=userid]')[0].selectedIndex,
-    reporterId = $('#j-editform select[name=userid] option').eq(selectedIndex).val(),
-    reporter = $('#j-editform select[name=userid] option').eq(selectedIndex).text();
+  var formData = formInfo['data'],
+    type = $('.j-type').data('type');
+  formData['id'] = urlData['reportId'];
+  formData['createTime'] = new Date(formData['createTime']).getTime();
+  var selectedIndex = $('#j-editform select[name=reporterId]')[0].selectedIndex,
+    reporterId = $('#j-editform select[name=reporterId] option').eq(selectedIndex).val(),
+    reporter = $('#j-editform select[name=reporterId] option').eq(selectedIndex).text();
   formData['reporterId'] = reporterId;
   formData['reporter'] = reporter;
-  var type = $('#j-editform select[name=type]').val();
   if (type == 1) {
     formData['pictures'] = '';
-  } else if (type == 2) {
-    formData['audio'] = '';
-  } else if (type == 3) {
-    formData['video'] = '';
-  }
-  $('#j-editform input[name=state]').each(function(){
-    if ($(this)[0].checked) {
-      formData['state'] = $(this).val();
+    $('.j-uploader .filelist .file').each(function(){
+      var fileurl = $(this).data('fileurl');
+      if (fileurl) {
+        if (formData['pictures'] == '') {
+          formData['pictures'] = fileurl;
+        } else {
+          formData['pictures'] += ',' + fileurl;
+        }
+      }
+    });
+  } else {
+    var fileurl = $('.j-uploader .filelist .file').eq(0).data('fileurl');
+    if (fileurl) {
+      if (type == 2) {
+        formData['audio'] = fileurl;
+      } else if (type == 4) {
+        formData['video'] = fileurl;
+      }
     }
-  });
+  }
   // 提交表单
-  lvsCmd.ajax('/live-web-cms/report/update.json', formData, function (state, res) {
+  lvsCmd.ajax(formInfo['url'], formData, function (state, res) {
     if (state) {
       if (res['status'] == '0') {
         alert("数据保存成功！");
@@ -119,22 +121,34 @@ newTplform.render(formConfig, function(){
 });
 
 // 获取数据
-lvsCmd.ajax('/live-web-cms/report/get.json', {reportId: id}, function (state, res) {
+lvsCmd.ajax('/live-web-cms/report/get.json', urlData, function (state, res) {
   if (state) {
     if (res['status'] == 0) {
       var formVal = res['data'];
       // 格式化日期
-      formVal['createtime'] = lvsCmd['formatDate'](formVal['createtime'], 'YY-MM-DD hh:mm');
+      formVal['createTime'] = lvsCmd['formatDate'](formVal['createTime'], 'YY-MM-DD hh:mm');
       // 插入报道人
-      $('#j-editform select[name=userid]').append('<option value="'+formVal['userid']+'">'+formVal['reporter']+'</option>');
+      $('#j-editform select[name=reporterId]').append('<option value="'+formVal['userId']+'">'+formVal['reporter']+'</option>');
       // 设置表单值
       newTplform.setval(formVal);
+      $('.j-type').data('type', formVal['type']);
       if (formVal['type'] == 1) {
+        $('.j-type input').val("图文");
         $('.j-uploader .filelist').data('filetype', 2);
       } else if (formVal['type'] == 2) {
+        $('.j-type input').val("音频");
         $('.j-uploader .filelist').data('filetype', 3);
       } else if (formVal['type'] == 4) {
+        $('.j-type input').val("视频");
         $('.j-uploader .filelist').data('filetype', 4);
+      }
+      // 创建上传组件
+      var newUpfile = new lvsCmd['upfile']($('.j-uploader .filelist'));
+      if (formVal['pictures']) {
+        var pictures = formVal['pictures'].split(',');
+        $.each(pictures, function(){
+          newUpfile.addfile(this);
+        });
       }
       // 绑定状态
       $('#j-editform .j-state input').each(function(){
@@ -150,12 +164,12 @@ lvsCmd.ajax('/live-web-cms/report/get.json', {reportId: id}, function (state, re
       lvsCmd.ajax('/live-web-cms/report/getReporters.json', {}, function (state, res) {
         if (state) {
           if (res['status'] == '0') {
-            var userid = $('#j-editform select[name=userid]').val();
-            $('#j-editform select[name=userid]').html('');
+            var userid = $('#j-editform select[name=reporterId]').val();
+            $('#j-editform select[name=reporterId]').html('');
             $.each(res['data'], function(){
-              $('#j-editform select[name=userid]').append('<option value="'+this['userId']+'">'+this['reporter']+'</option>');
+              $('#j-editform select[name=reporterId]').append('<option value="'+this['userId']+'">'+this['reporter']+'</option>');
             });
-            newTplform.setval({userid:userid});
+            newTplform.setval({reporterId:userid});
           }
         }
       });
